70d6e7f9ea7d676f2f8b4bf39d0099d309e9d768
Cristian Alderete <CAlderete@TECNOSTAFF.LOCAL>
2021-06-29 15:04:12 -0300

    merging vs repo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HX_Ferrari_Programmer
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
